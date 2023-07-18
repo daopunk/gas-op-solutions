06459f2bf1f9a7b664e3dd1a239e6d7a1373db82
Hunter King <hking6776@gmail.com>
2023-07-17 23:18:35 -0500

    distribute gas reduction

--- contracts/optimized_contracts/Distribute.sol
+++ contracts/optimized_contracts/Distribute.sol
@@ -2,22 +2,30 @@
 pragma solidity 0.8.15;
 
 contract OptimizedDistribute {
-    address[4] public contributors;
-    uint256 public createTime;
+    address immutable contr1;
+    address immutable contr2;
+    address immutable contr3;
+    address immutable contr4;
+
+    uint256 immutable createTime;
+
+    uint256 immutable amount;
 
     constructor(address[4] memory _contributors) payable {
-        contributors = _contributors;
         createTime = block.timestamp + 1 weeks;
+        contr1 = _contributors[0];
+        contr2 = _contributors[1];
+        contr3 = _contributors[2];
+        contr4 = _contributors[3];
+        amount = msg.value >> 2;
     }
 
     function distribute() external {
         require(block.timestamp > createTime, "cannot distribute yet");
 
-        uint256 amount = address(this).balance >> 2;
-
-        payable(contributors[0]).call{value: amount}("");
-        payable(contributors[1]).call{value: amount}("");
-        payable(contributors[2]).call{value: amount}("");
-        payable(contributors[3]).call{value: amount}("");
+        payable(contr1).call{value: amount}("");
+        payable(contr2).call{value: amount}("");
+        payable(contr3).call{value: amount}("");
+        payable(contr4).call{value: amount}("");
     }
 }
